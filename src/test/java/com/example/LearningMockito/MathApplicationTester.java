2f27d917054407d6507e19a7dae60b2eb23b9513
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-07-04 15:58:12 +0100

    Add notes for Mockito - Exception Handling and create new testAdd() method in MathApplicationTester. /Old code was commented out/

--- src/test/java/com/example/LearningMockito/MathApplicationTester.java
+++ src/test/java/com/example/LearningMockito/MathApplicationTester.java
@@ -28,32 +28,42 @@ public class MathApplicationTester {
     @Mock
     CalculatorService calcService;
 
-    @DisplayName("Testing add functionality")
-    @Test
+//    @DisplayName("Testing add functionality")
+//    @Test
+//    public void testAdd() {
+////      add the behavior of calc service to add two numbers
+//        when(calcService.add(10.0, 20.0)).thenReturn(30.0);
+//
+////      add the behavior of calc service to subtract two numbers
+//        when(calcService.subtract(20.0,10.0)).thenReturn(10.00);
+//
+////      test the add functionality
+//        assertEquals(mathApplication.add(10, 20), 30);
+//        assertEquals(mathApplication.add(10, 20), 30);
+//        assertEquals(mathApplication.add(10, 20), 30);
+//
+////      test the subtract functionality
+//        assertEquals(mathApplication.subtract(20, 10), 10);
+//
+////      check a minimum 1 call count
+//        verify(calcService, atLeastOnce()).add(10, 20);
+//
+////      check if add function is called minimum 2 times
+//        verify(calcService, atLeast(2)).add(10, 20);
+//
+////      check if add function is called maximum 3 times
+//        verify(calcService, atMost(3)).add(10.0,20.0);
+//    }
+//
+    @Test(expected = RuntimeException.class)
     public void testAdd() {
-//      add the behavior of calc service to add two numbers
-        when(calcService.add(10.0, 20.0)).thenReturn(30.0);
 
-//      add the behavior of calc service to subtract two numbers
-        when(calcService.subtract(20.0,10.0)).thenReturn(10.00);
+//      add the behavior to throw exception
+        doThrow(new RuntimeException("Add operation not implement"))
+                .when(calcService.add(10,20));
 
-//      test the add functionality
         assertEquals(mathApplication.add(10, 20), 30);
-        assertEquals(mathApplication.add(10, 20), 30);
-        assertEquals(mathApplication.add(10, 20), 30);
-
-//      test the subtract functionality
-        assertEquals(mathApplication.subtract(20, 10), 10);
-
-//      check a minimum 1 call count
-        verify(calcService, atLeastOnce()).add(10, 20);
-
-//      check if add function is called minimum 2 times
-        verify(calcService, atLeast(2)).add(10, 20);
-
-//      check if add function is called maximum 3 times
-        verify(calcService, atMost(3)).add(10.0,20.0);
-
     }
+
 
 }
